1c3169112c4dda511ea0cc82efb6ae6ebb80a0b8
Ben <ben@ben-rush.net>
2017-07-08 23:22:07 -0500

    Gives recommendations now.

--- NormalizationModule/app/views.py
+++ NormalizationModule/app/views.py
@@ -8,53 +8,51 @@ from django.template import RequestContext
 from datetime import datetime
 from os import path, getcwd
 import NormalizationModule.mark2cure.dataaccess
+import NormalizationModule.mark2cure.matcher
 from NormalizationModule.mark2cure.matcher import MeshRecord, FindRecommendations, Mark2CureQuery, ReadMeshRecordsFromDisk, TFIDF
 #import app.mark2cure.matcher
-
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
-    annotationText, passageText = NormalizationModule.mark2cure.dataaccess.GetRandomAnnotation()
+    passageText, annotationText = NormalizationModule.mark2cure.dataaccess.GetRandomAnnotation()
 
     desciptorPath = path.join(getcwd(), 'descriptors.pickle')
 
-    query = Mark2CureQuery(annotationText, passageText)
+    query = Mark2CureQuery(annotationText[0], passageText[0])
     meshRecords = ReadMeshRecordsFromDisk(desciptorPath)
     tfidf = TFIDF()
     tfidf.TrainModel(meshRecords)
     recommendations = FindRecommendations(query, meshRecords, tfidf, 4)
 
-    return render(
-        request,
+    match = "None found"
+    if len(recommendations) > 0:
+        match = recommendations[0].MainLine
+
+    return render(request,
         'app/index.html',
         {
             'title':annotationText,
-            'message':recommendations[0]
-        }
-    )
+            'match':match
+        })
 
 def contact(request):
     """Renders the contact page."""
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
+    return render(request,
         'app/contact.html',
         {
             'title':'Contact',
             'message':'Your contact page.',
             'year':datetime.now().year,
-        }
-    )
+        })
 
 def about(request):
     """Renders the about page."""
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
+    return render(request,
         'app/about.html',
         {
             'title':'About',
             'message':'Your application description page.',
             'year':datetime.now().year,
-        }
-    )
+        })
